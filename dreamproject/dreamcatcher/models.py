4dcb8721921a972ba6506dd20c3d218d160f900b
Katrina Florendo <kflorendo1012@gmail.com>
2024-01-13 13:05:53 -0500

    Import User into models

--- dreamproject/dreamcatcher/models.py
+++ dreamproject/dreamcatcher/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class DreamSequence(models.Model):
     user = models.ForeignKey(User, default=None, on_delete=models.PROTECT)
