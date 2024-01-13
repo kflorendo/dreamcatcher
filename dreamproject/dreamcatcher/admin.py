58ab40443ec489ad6ccc760784d21d5ee9c665e5
nathan-marquez <nquezt@gmail.com>
2024-01-13 16:43:12 -0500

    [Admin] install models

--- dreamproject/dreamcatcher/admin.py
+++ dreamproject/dreamcatcher/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import DreamSequence, DreamChunk, Profile
+
+admin.site.register(DreamSequence)
+admin.site.register(DreamChunk)
+admin.site.register(Profile)
